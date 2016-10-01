1df72aa9ce285b1bd0049c6f15c77204e7d994b7
Matteo Pagani <matteo.pagani@microsoft.com>
2016-10-01 21:45:00 +0200

    ﻿Chenged the project to leverage a managed .NET library which uses P/Invoke instead of a native C++ library

---
+++ DesktopBridge.Helpers/Helpers.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Text;
+
+namespace DesktopBridge
+{
+    public class Helpers
+    {
+        [DllImport("kernel32.dll", CharSet = CharSet.Unicode, SetLastError = true)]
+        static extern int GetCurrentPackageFullName(ref int packageFullNameLength, ref StringBuilder packageFullName);
+
+        public bool IsRunningAsUwp()
+        {
+            if (isWindows7OrLower())
+            {
+                return false;
+            }
+            else
+            {
+                StringBuilder sb = new StringBuilder(1024);
+                int length = 0;
+                int result = GetCurrentPackageFullName(ref length, ref sb);
+
+                return result != 15700;
+            }
+        }
+
+        private bool isWindows7OrLower()
+        {
+            int versionMajor = Environment.OSVersion.Version.Major;
+            int versionMinor = Environment.OSVersion.Version.Minor;
+            double version = versionMajor + (double)versionMinor / 10;
+            return version <= 6.1;
+        }
+    }
+}
